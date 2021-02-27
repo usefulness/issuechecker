982397afcdd454a523ca617248cd3d795182face
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-02-27 12:17:32 +0100

    Add javadoc publishing

--- buildSrc/src/main/kotlin/PublishingPlugin.kt
+++ buildSrc/src/main/kotlin/PublishingPlugin.kt
@@ -14,6 +14,7 @@ class PublishingPlugin : Plugin<Project> {
 
         extensions.configure<JavaPluginExtension> {
             withSourcesJar()
+            withJavadocJar()
         }
         extensions.configure<PublishingExtension> {
             with(repositories) {
