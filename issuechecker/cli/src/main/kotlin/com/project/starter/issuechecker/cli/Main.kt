15347e47522931eb437ef9994c76ced4d27e247d
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2024-09-21 08:13:18 +0200

    Add missing import

--- issuechecker/cli/src/main/kotlin/com/project/starter/issuechecker/cli/Main.kt
+++ issuechecker/cli/src/main/kotlin/com/project/starter/issuechecker/cli/Main.kt
@@ -3,6 +3,7 @@
 package com.project.starter.issuechecker.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.split
